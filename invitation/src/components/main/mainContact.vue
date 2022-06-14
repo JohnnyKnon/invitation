fb71a7dbc93509791ec7723f1ec8c8ee04dc03f3
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-06-14 14:25:05 +0900

    문의하기 파트 끝

--- invitation/src/components/main/mainContact.vue
+++ invitation/src/components/main/mainContact.vue
@@ -1,17 +1,17 @@
 <template>
     <section id="contact">
         <!-- request pop up -->
-        <section class="request-wrapper" :class="{requestBG : requestActive}">
+        <section class="request-wrapper" :class="{requestBGActive : requestActive}">
             <!-- form -->
-             <form class="request-form" :class="{requestForm : requestActive}">
+             <form class="request-form" :class="{requestFormActive : requestActive}">
                 <!-- times -->
-                <i class="fas fa-times"></i>
+                <i class="fas fa-times" @click="requestActive = false"></i>
                 <!-- if request to comment v-if="requestItemVind === 'delete'"   v-else-if="requestItemVind === 'comment'" v-else-if="requestItemVind === 'qna'"-->
-                <section id="request-delete" >
+                <section id="request-delete" v-if="requestItemVind === 'delete'">
                     <!-- items title -->
                     <header class="request-items-title"><h1>신청 취소하기</h1></header>
                     <!-- items contents -->
-                    <section class="delete-agree">
+                    <section class="agree delete-agree">
                         <label for="delete-agree-text">개인정보 수집 및 이용 동의</label>
                         <textarea name="deleteAgree" id="delete-agree-text" readonly>
 1. 수집하는 개인정보 항목
@@ -55,14 +55,58 @@
                         <input type="email" name="deleteEmail" id="delete-email" placeholder="예) example@mossic.com" required>
                         <span class="highlight">* 취소여부는 해당 이메일로 전송됩니다. *</span>
                     </section>
-                    <section class="request-delete-submit">
+                    <section class="request-submit">
                         <input type="submit" value="신청취소하기">
                     </section>
                 </section>
-                <section id="notOpen">
-
+                <!-- Not open -->
+                <section id="notOpen" v-else-if="requestItemVind === 'notOpen'">
+                    <h1>Sorry..</h1>
+                    <h2>해당 서비스는 아직 지원하지 않습니다.</h2>
                 </section>
-                <section id="request-qna" >
+                <!-- qna -->
+                <section id="request-qna"  v-else-if="requestItemVind === 'qna'">
+                    <header class="request-items-title"><h1>문의하기</h1></header>
+                    <section class="agree qna-agree">
+                        <label for="qna-agree-text">개인정보 수집 및 이용 동의</label>
+                        <textarea name="qnaAgree" id="qna-agree-text" readonly>
+1. 수집하는 개인정보 항목
+본 사이트는 온라인 상담을 위해 아래와 같은 개인정보를 수집하고 있습니다.
+수집항목: 이름, 전화번호, 이메일
+개인정보 수집방법: 문의하기
+
+2. 개인정보의 수집 및 이용목적
+회사는 수집한 개인정보를 다음의 목적을 위해 사용합니다.
+문의하기
+
+3. 개인정보의 보유 및 이용기간
+원칙적으로, 개인정보 수집 및 이용목적이 달성된 후에는 해당 정보를 지체 없이 파기합니다.
+                        </textarea>
+                        <div class="agree-check">
+                            <input type="checkbox" name="qnaCheck" id="qna-check" class="checkbox" required>
+                            <label for="qna-check">동의하시겠습니까?</label>
+                        </div>
+                        <section class="request-total-style request-delete-name">
+                            <label for="qna-name">신청자명</label>
+                            <input type="text" name="qnaName" id="qna-name" placeholder="예) 홍길동" maxlength="15" required>
+                        </section>
+                        <section class="request-total-style request-qna-tel">
+                            <label for="qna-tel">신청자 전화번호</label>
+                            <input type="tel" name="qnaTel" id="qna-tel" placeholder="예) 01012341234" required>
+                        </section>
+                        <section class="request-total-style request-delete-email">
+                            <label for="qna-email">신청자 이메일</label>
+                            <input type="email" name="qnaEmail" id="qna-email" placeholder="예) example@mossic.com" required>
+                        </section>
+                        <section class="request-total-style request-qna-content">
+                            <label for="qna-contents">내용</label>
+                            <textarea name="qnaContents" id="qna-contents"></textarea>
+                            <span class="highlight">* 문의는 해당 이메일로도 전송됩니다. *</span>
+                        </section>
+                        <section class="request-submit">
+                            <input type="submit" value="문의하기">
+                        </section>
+                    </section>
                 </section>
             </form>
         </section>
@@ -177,6 +221,17 @@ export default {
     height: 100%;
     // background
     background-color: var(--light-sub-color);
+     //  active
+    .requestBGActive {
+        // opacity
+        opacity: 1!important;
+        visibility: visible!important;
+        // form acitve
+        .requestFormActive{
+            // transform
+            transform: translateY(0)!important;
+        }
+    }
     // request wrapper
     .request-wrapper{
         // position
@@ -187,17 +242,18 @@ export default {
         width: 100%;
         height: 100%;
         // background
-        background-color: rgba(0, 0, 0, 0.454);
+        background-color: rgba(0, 0, 0, 0.585);
         // opacity
-        opacity: 1;
-        // visible
-        visibility: visible;
+        opacity: 0;
+        visibility: hidden;
         // z-index
         z-index: 1000;
         // flex
         display: flex;
         justify-content: center;
         align-items: center;
+        // transition
+        transition: all 350ms ease-in-out;
         // form
         .request-form{
             // position
@@ -209,6 +265,10 @@ export default {
             background-color: #fff;
             // border
             border-radius: 30px;
+            // transform
+            transform: translateY(-2000px);
+            // transition
+            transition: all 350ms ease-in-out;
             @include ResponsiveMobile(){
                 // size
                 width: 90%;
@@ -222,8 +282,20 @@ export default {
                 position: absolute;
                 top: 30px;
                 right: 35px;
+                z-index: 1000;
                 // cursor
                 cursor: pointer;
+            }
+             // title
+            .request-items-title{
+                    // font
+                    font-family: var(--font-kr);
+                    font-size: 1.8rem;
+                    color: var(--dark-sub-color);
+                    @include ResponsiveMobile(){
+                        // font
+                        font-size: 1.5rem;
+                    }
             }
             // total style input & select
             .request-total-style{
@@ -268,91 +340,7 @@ export default {
                         height: 30px;
                     }
             }
-            // Delete Request
-            #request-delete{
-                // font
-                font-family: var(--font-kr);
-                // position
-                position: absolute;
-                top: 20%;
-                left: 50%;
-                // size
-                width: 90%;
-                height: 90%;
-                // transfrom
-                transform: translate(-50%, -20%);
-                // flex
-                display: flex;
-                flex-direction: column;
-                align-items: flex-start;
-                gap: 10px;
-                @include ResponsiveMobile(){
-                    // flex
-                    gap: 10px;
-                }
-                // title
-                .request-items-title{
-                    // font
-                    font-family: var(--font-kr);
-                    font-size: 1.8rem;
-                    color: var(--dark-sub-color);
-                    @include ResponsiveMobile(){
-                        // font
-                        font-size: 1.5rem;
-                    }
-                }
-                // delete agree
-                .delete-agree{
-                    // size
-                    width: 100%;
-                    // flex
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    gap: 10px;
-                    @include ResponsiveMobile(){
-                        // margin
-                        margin-top: -20px;
-                    }
-                    // label
-                    label{
-                        @include ResponsiveMobile(){
-                            // font
-                            font-size: 0.8rem;
-                        }
-                    }
-                    // textarea
-                    textarea{
-                        // font
-                        font-family: var(--font-kr);
-                        font-size: 0.8rem;
-                        color: rgb(92, 92, 92);
-                        // text
-                        text-align: justify;
-                        // size
-                        width: 100%;
-                        height: 100px;
-                        // resize
-                        resize: none;
-                        @include ResponsiveMobile(){
-                            height: 60px;
-                        }
-                    }
-                    .agree-check{
-                        // flex
-                        display: flex;
-                        align-items:center;
-                        label{
-                            // font
-                            font-size: 0.8rem;
-                            // cursor
-                            cursor: pointer;
-                        }
-                    }
-                }
-                // request delete style
-                
-                .request-delete-submit{
+            .request-submit{
                     // margin
                     margin-top: 10px;
                     // size
@@ -375,9 +363,143 @@ export default {
                         cursor: pointer;
                     }
                 }
+             // agree
+            .agree{
+                // size
+                width: 100%;
+                // flex
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                gap: 10px;
+                @include ResponsiveMobile(){
+                    // margin
+                    margin-top: -20px;
+                }
+                // label
+                label{
+                    @include ResponsiveMobile(){
+                        // font
+                        font-size: 0.8rem;
+                    }
+                }
+                // textarea
+                textarea{
+                    // font
+                    font-family: var(--font-kr);
+                    font-size: 0.8rem;
+                    color: rgb(92, 92, 92);
+                    // text
+                    text-align: justify;
+                    // size
+                    width: 100%;
+                    height: 100px;
+                    // resize
+                    resize: none;
+                    @include ResponsiveMobile(){
+                        height: 60px;
+                    }
+                }
+                .agree-check{
+                        // flex
+                        display: flex;
+                        align-items:center;
+                    label{
+                         // font
+                        font-size: 0.8rem;
+                        // cursor
+                        cursor: pointer;
+                    }
+                }
+            }
+            // Delete Request
+            #request-delete{
+                // font
+                font-family: var(--font-kr);
+                // position
+                position: absolute;
+                top: 20%;
+                left: 50%;
+                // size
+                width: 90%;
+                height: 90%;
+                // transfrom
+                transform: translate(-50%, -20%);
+                // flex
+                display: flex;
+                flex-direction: column;
+                align-items: flex-start;
+                gap: 10px;
+                @include ResponsiveMobile(){
+                    // flex
+                    gap: 10px;
+                }
+            }
+            // NotOpen
+            #notOpen{
+                // position
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                // size
+                width: 90%;
+                height: 90%;
+                // transform
+                transform: translate(-50%, -50%);
+                // flex
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                h1{
+                    // font
+                    font-family: var(--font-eng);
+                    font-size: 3rem;
+                    color: var(--dark-sub-color);
+                }
+                h2{
+                    // font
+                    font-family: var(--font-kr);
+                    font-size: 1rem;
+                    color: var(--sub-color);
+                    // margin
+                    margin-top: -40px ;
+                }
+            }
+            // qna
+            #request-qna{
+                // font
+                font-family: var(--font-kr);
+                // position
+                position: absolute;
+                top: 20%;
+                left: 50%;
+                // size
+                width: 90%;
+                height: 90%;
+                // transfrom
+                transform: translate(-50%, -20%);
+                // flex
+                display: flex;
+                flex-direction: column;
+                align-items: flex-start;
+                gap: 10px;
+                // qna-contents
+                .request-qna-content{
+                    // size
+                    width: 100%;
+                    // textarea
+                    textarea{
+                        // size
+                        width: 95%;
+                        @include ResponsiveMobile(){
+                            // height
+                            height: 60px;
+                        }
+                    }
+                }
             }
         }
-
     }
     // contact wrapper
     .contact-wrapper{
