748ac46773466a8f5e75863c420c55c600810adc
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-06-15 16:05:44 +0900

    서브 네비 모바일 완료

--- invitation/src/components/subDefault/subNav.vue
+++ invitation/src/components/subDefault/subNav.vue
@@ -3,7 +3,7 @@
         <ul class="subNav-items-wrapper">
             <li class="subNav-items">
                 <!-- top category -->
-                <a href="#" class="top-item">모바일 초대장</a>
+                <a href="#" class="top-item">모바일 초대장 <span class="more"><i class="fa-solid fa-angle-down"></i></span></a>
                 <!-- bottom -->
                 <ul class="bottom-items-wrapper">
                     <li class="bottom-item">
@@ -26,11 +26,6 @@
                 <a href="#" class="top-item">모바일 부고장</a>
             </li>
         </ul>
-        <div class="subNav-toggle-wrapper">
-            <span class="top"></span>
-            <span class="middle"></span>
-            <span class="bottom"></span>
-        </div>
     </nav>
 </template>
 <script>
@@ -84,6 +79,14 @@ export default {
         display: flex;
         justify-content: space-around;
         align-items: center;
+         @include ResponsiveMobile() {
+             // size
+            width: 80%;
+            // flex
+             flex-direction: column;
+             align-items: flex-start;
+             gap: 25px;
+         }
         .subNav-items{
             // position
             position: relative;
@@ -91,6 +94,15 @@ export default {
                 // font
                 font-size: 1.1rem;
                 color: #fff;
+                // more
+                .more{
+                    // display
+                    display: none;
+                    @include ResponsiveMobile() {
+                        // display
+                        display: inline-block;
+                    }
+                }
             }
             // bottom
             .bottom-items-wrapper{
@@ -114,20 +126,66 @@ export default {
                 visibility: hidden;
                  // transition
                 transition: all 250ms ease-in-out;
+                 @include ResponsiveMobile() {
+                        //  position
+                        position: relative;
+                        top: 10px;
+                        left: 0;
+                        // padding
+                        padding: 0;
+                        // background
+                        background-color: transparent;
+                        // flex
+                        display: none;
+                        flex-direction: column;
+                        justify-content: center;
+                        align-items: flex-start;
+                        gap: 10px;
+                        // opacity
+                        opacity: 1;
+                        // visible
+                        visibility: visible;
+                         // transform
+                        transform: translateX(0) translateY(0);
+                        
+                 }
                 // bottom item
                 .bottom-item{
                     a{
                         // font
                         font-size: 0.9rem;
                         color: #fff;
+                        @include ResponsiveMobile() {
+                            // size
+                            width: 100px;
+                        }
                     }
                 }
                 @for $i from 1 through 2 {
                     .bottom-item:nth-child(#{$i}){
+                        // size
+                        width: 100%;
                         // padding
                         padding-bottom: 10px;
                         // border
-                        border-bottom: 1px dotted var(--light-grey-color);
+                        border-bottom: 1px solid var(--light-grey-color);
+                        @include ResponsiveMobile() {
+                            // text
+                            text-align: left;
+                            // padding
+                            padding-bottom: 0;
+
+                        }
+                    }
+                }
+                .bottom-item:nth-child(3){
+                    @include ResponsiveMobile() {
+                         // text
+                        text-align: left;
+                         // size
+                        width: 100%;
+                        // border
+                        border-bottom: 1px solid var(--light-grey-color);
                     }
                 }
                 .bottom-item:nth-child(2),
@@ -135,6 +193,10 @@ export default {
                 {
                     // padding
                     padding-top: 8px;
+                    @include ResponsiveMobile() {
+                         // padding
+                        padding-top: 0;
+                    }
                 }
             }
             // bottom hover
@@ -147,13 +209,20 @@ export default {
                     opacity: 1;
                     // visible
                     visibility: visible;
+                    @include ResponsiveMobile() {
+                        // display
+                        display: flex;
+                        // transform
+                        transform: translateX(0) translateY(0);
+                        // opacity
+                        opacity: 1;
+                        // visible
+                        visibility: visible;
+                    }
+                
                 }
             }
         }
-    }
-    .subNav-toggle-wrapper{
-        // display
-        display: none;
     }
 }
 
